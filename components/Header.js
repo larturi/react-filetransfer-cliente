d390f2a61880371890103cff26a8e4b37e996d3d
lea.arturi <lea.arturi@gmail.com>
2021-04-24 15:59:07 -0300

    Avances

--- components/Header.js
+++ components/Header.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import Link from 'next/link';
 
 const Header = () => {
     return (
-        <header className="py-8 flex flex-col md:flex-row items-center justyfy-between">
-           <img className="w-64 mb-8 md:mb-0" src="logo.png"/>
+        <header className="py-8 flex flex-col md:flex-row items-center justify-between">
+           <Link href="/">
+                <img className="w-64 mb-8 md:mb-0" src="logo.png"/>
+           </Link>
+
+           <div>
+                <Link href="/login">
+                   <a className="bg-red-500 px-5 py-3 rounded-lg text-white font-bold uppercase mr-2">Iniciar Sesion</a>
+                </Link>
+
+                <Link href="/crearcuenta">
+                   <a className="bg-black px-5 py-3 rounded-lg text-white font-bold uppercase">Crear Cuenta</a>
+                </Link>
+           </div>
         </header>
     )
 }
--- pages/crearcuenta.js
+++ pages/crearcuenta.js
@@ -4,15 +4,17 @@ import * as Yup from 'yup';
 
 import Layout from '../components/Layout';
 
-const Login = () => {
+const CrearCuenta = () => {
 
   // Formulario y validacion con Formik y Yup
   const formik = useFormik({
     initialValues: {
+      nombre: '',
       email: '',
       password: ''
     },
     validationSchema: Yup.object({
+      nombre:   Yup.string().required('El nombre es obligatorio'),
       email:    Yup.string().email('El email no es valido').required('El email es obligatorio'),
       password: Yup.string().required('El password es obligatorio').min(6, 'El password debe contener al menos 6 caracteres'),
     }),
@@ -24,7 +26,7 @@ const Login = () => {
   return (
     <Layout>
       <div className="md:w-4/5 xl:w-3/5 mx-auto mb-32">
-        <h2 className="text-4xl font-sans font-bold text-gray-800 text-center my-4">Login</h2>
+        <h2 className="text-4xl font-sans font-bold text-gray-800 text-center my-4">Crear Cuenta</h2>
 
         <div className="flex justify-center mt-5">
           <div className="w-full max-w-lg">
@@ -32,6 +34,24 @@ const Login = () => {
               className="bg-white rounded shadow-md px-8 pt-6 pb-8 mb-4"
               onSubmit={formik.handleSubmit}
             >
+              <div className="mb-4">
+                <label className="block text-black text-sm font-bold mb-2" htmlFor="nombre">Nombre</label>
+                <input 
+                  type="text"
+                  className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 focus:ring-2 focus:ring-indigo-200 focus:outline-none focus:shadow-outline"
+                  id="nombre"
+                  placeholder="Nombre de Usuario"
+                  value={formik.values.nombre}
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
+                />
+                {formik.touched.nombre && formik.errors.nombre ? (
+                  <div className="my-2 bg-gray-200 border-l-4 border-red-500 text-red-700 p-4">
+                    <p>{formik.errors.nombre}</p>
+                  </div>
+                ) : null}
+              </div>
+
               <div className="mb-4">
                 <label className="block text-black text-sm font-bold mb-2" htmlFor="email">Email</label>
                 <input 
@@ -71,7 +91,7 @@ const Login = () => {
               <input
                 type="submit"
                 className="bg-red-500 hover:bg-gray-900 w-full p-2 text-white uppercase font-bold"
-                value="Iniciar Sesion"
+                value="Crear Cuenta"
               />
             </form>
           </div>
@@ -81,4 +101,4 @@ const Login = () => {
   )
 }
 
-export default Login;
+export default CrearCuenta;
