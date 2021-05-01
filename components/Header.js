ed7eab5e2cd86c94c0e227c728549c4eea2d5cce
lea.arturi <lea.arturi@gmail.com>
2021-05-01 17:35:35 -0300

    Logo

--- components/Header.js
+++ components/Header.js
@@ -30,7 +30,7 @@ const Header = () => {
        <header className="py-8 flex flex-col md:flex-row items-center justify-between">
            <img 
             className="w-64 mb-8 md:mb-0 cursor-pointer" 
-            src="/logo.svg"
+            src="/logo.png"
             onClick={ () => goToIndex() }
          />
 
--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 module.exports = {
+    // Local
+    // env: {
+    //     backendURL: 'http://localhost:4000',
+    //     frontendURL: 'http://localhost:3000'
+    // }
+
+    // Prod
     env: {
-        backendURL: 'http://localhost:4000',
-        frontendURL: 'http://localhost:3000'
+        backendURL: 'https://fast-mountain-82209.herokuapp.com',
+        frontendURL: 'https://react-filetransfer-cliente.vercel.app'
     }
 }
