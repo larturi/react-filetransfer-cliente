6e65dc59a3cfdc9704f1f476481fcaaea5ea3cd3
lea.arturi <lea.arturi@gmail.com>
2021-07-18 18:04:51 -0300

    Cambio rutas en env

--- next.config.js
+++ next.config.js
@@ -1,13 +1,20 @@
 module.exports = {
-    // Local
-    // env: {
-    //     backendURL: 'http://localhost:4000',
-    //     frontendURL: 'http://localhost:3000'
-    // }
+   // Local
+   // env: {
+   //     backendURL: 'http://localhost:4000',
+   //     frontendURL: 'http://localhost:3000'
+   // }
 
-    // Prod
-    env: {
-        backendURL: 'http://lab.apirest.com.ar:3005',
-        frontendURL: 'http://lab.apirest.com.ar:3006'
-    }
-}
+   // Prod en DonWeb
+   // env: {
+   //     backendURL: 'http://lab.apirest.com.ar:3005',
+   //     frontendURL: 'http://lab.apirest.com.ar:3006'
+   // }
+
+   // Prod en Heroku y Vercel
+   env: {
+      backendURL: 'https://node-filetransfer-server.herokuapp.com',
+      frontendURL:
+         'https://react-filetransfer-cliente-8tqnggd3g-larturi.vercel.app',
+   },
+};
