d3c177f59f9534b469ffff20cd13a0dfc423d4e5
Leandro Arturi <lea.arturi@gmail.com>
2023-01-31 22:34:39 -0300

    Readme

--- next.config.js
+++ next.config.js
@@ -1,19 +1,7 @@
 module.exports = {
    // Local
-   // env: {
-   //     backendURL: 'http://localhost:4000',
-   //     frontendURL: 'http://localhost:3000'
-   // }
-
-   // Prod en DonWeb
-   // env: {
-   //     backendURL: 'http://lab.apirest.com.ar:3005',
-   //     frontendURL: 'http://lab.apirest.com.ar:3006'
-   // }
-
-   // Prod en Heroku y Vercel
    env: {
-      backendURL: 'https://node-filetransfer-server.herokuapp.com',
-      frontendURL: 'https://react-filetransfer-cliente.vercel.app',
-   },
+       backendURL: 'http://localhost:4000',
+       frontendURL: 'http://localhost:3000'
+   }
 };
